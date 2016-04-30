fd518c8b39b45be2175b67331eff1ba1be005bbd
Jorge Valencia <jorgev@jorgev.es>
2016-05-01 01:57:26 +0200

    FIrst functional version for use on command line
    added if sentence for preserve not altitude data from being modified by
    script
    Correction setup to -55 , Looks fine on my traceklogs whit this

--- as.sh
+++ as.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/awk -f
 
-if (x % 2 == 0) 
-    BEGIN { FS = "," ;ARGC = 3; ARGV[1] = "vuelo.kml" }
-  	{ $3 = $3 - 68 }
-  		{ print $1","$2","$3 }
+    BEGIN 	{ FS = "," ;ARGC = 3; ARGV[1] = "vuelo.kml" }
+    
+  	{ $3 = $3 - 55 }
+	  {if ($1 ~ /^-/)	  
+  print $1","$2","$3  
+else
+	print $1
 	
-		; else
-       print "else executed"
-	   
- 
+}	
